4915d1bd3aec6627a6b0fb0a7b3d6a156741594b
JosueV4 <Yosue4422@gmail.com>
2022-09-09 15:11:33 -0500

    Creación Login

---
+++ src/main/java/controller/UsuarioController.java
@@ -0,0 +1,12 @@
+
+package controller;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import beans.Usuario;
+import connection.DBConnection;
+
+public class UsuarioController implements IUsuarioController {
+    public String login(String )
+    
+}
